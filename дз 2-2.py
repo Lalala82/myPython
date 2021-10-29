a45b78a63606fd31006f775ff2ccb0e2fa954378
Lalala82 <93260938+Lalala82@users.noreply.github.com>
2021-10-29 21:10:54 +0300

    Add files via upload

---
+++ дз 2-2.py
@@ -0,0 +1,39 @@
+a = 'Good day, teacher'
+print(type(a), a)
+
+b = 39
+print(type(b), b)
+
+c = 1.2
+print(type(c), c)
+
+d = bytes(4)
+print(type(d), d)
+
+e = [1, 2, 4, a, b, c]
+print(type(e), e)
+
+f = (1, 30, 100, 250)
+print(type(f), f)
+
+j = {'1', '2', '20'}
+print(type(j), j)
+
+h = frozenset({'good', 'bad', 20})
+print(type(h), h)
+
+i = {1 : 'teacher', 2 : 'pupil'}
+print(type(i), i)
+
+q = 'Go'
+w = 'home'
+z = q + w
+print(z)
+
+a = 'exersise'
+b = 2
+print(a, b)
+
+a = 'exersise'
+b = 2
+print(a + str(b))
